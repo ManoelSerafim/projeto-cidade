3cf430270e20a0e0e2143793176b86330869d11b
Joaoemanuelx <joaoxavier043@gmail.com>
2026-05-10 22:45:07 -0300

    correção na implementação do banco de dados

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProblemasModule } from './problemas/problemas.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
   imports: [ProblemasModule, TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'db.tiinhdeorwhhxymrjoln.supabase.co',
