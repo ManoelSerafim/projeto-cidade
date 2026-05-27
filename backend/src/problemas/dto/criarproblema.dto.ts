891bd3dc555848afad3d3db8bfe3d16aa20526b9
Joaoemanuelx <joaoxavier043@gmail.com>
2026-05-27 01:38:46 -0300

    implementação do pipevalidation

--- backend/src/problemas/dto/criarproblema.dto.ts
+++ backend/src/problemas/dto/criarproblema.dto.ts
@@ -1,8 +1,34 @@
+import { IsNotEmpty, IsNumber, IsString, Min, Max, IsUUID} from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CriarProblemaDto {
+    @IsNotEmpty()
+    @IsString()
     problem_title!: string;
+
+    @IsNotEmpty()
+    @IsUUID()
     user_id!: string;
+
+    @IsNotEmpty()
+    @IsString()
     problem_gravity!: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @Type(() => Number)
+    @Max(90)
+    @Min(-90)
     problem_latitude!: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @Type(() => Number)
+    @Max(180)
+    @Min(-180)
     problem_longituded!: number;
+
+    @IsNotEmpty()
+    @IsUUID()
     categories_id!: string;
 }
